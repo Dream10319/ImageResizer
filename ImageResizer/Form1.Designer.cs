61108168bb318a9177c6d4458f090f44071a18d8
TomLove <lovetom.pro@gmail.com>
2024-12-10 03:25:44 +0800

    add all function

--- ImageResizer/Form1.Designer.cs
+++ ImageResizer/Form1.Designer.cs
@@ -37,7 +37,7 @@
             this.desDir = new System.Windows.Forms.TextBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
-            this.button3 = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.label3 = new System.Windows.Forms.Label();
@@ -117,6 +117,7 @@
             this.button1.TabIndex = 3;
             this.button1.Text = "...";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -126,6 +127,7 @@
             this.button2.TabIndex = 5;
             this.button2.Text = "...";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // desDir
             // 
@@ -154,14 +156,15 @@
             this.progressBar1.Size = new System.Drawing.Size(423, 23);
             this.progressBar1.TabIndex = 1;
             // 
-            // button3
+            // btnStart
             // 
-            this.button3.Location = new System.Drawing.Point(239, 250);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Start";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnStart.Location = new System.Drawing.Point(239, 250);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
             // 
             // button4
             // 
@@ -206,6 +209,11 @@
             this.width1.Name = "width1";
             this.width1.Size = new System.Drawing.Size(54, 20);
             this.width1.TabIndex = 1;
+            this.width1.Value = new decimal(new int[] {
+            1040,
+            0,
+            0,
+            0});
             // 
             // height1
             // 
@@ -218,6 +226,11 @@
             this.height1.Name = "height1";
             this.height1.Size = new System.Drawing.Size(54, 20);
             this.height1.TabIndex = 2;
+            this.height1.Value = new decimal(new int[] {
+            1456,
+            0,
+            0,
+            0});
             // 
             // label4
             // 
@@ -252,6 +265,11 @@
             this.height2.Name = "height2";
             this.height2.Size = new System.Drawing.Size(54, 20);
             this.height2.TabIndex = 2;
+            this.height2.Value = new decimal(new int[] {
+            910,
+            0,
+            0,
+            0});
             // 
             // label5
             // 
@@ -273,6 +291,11 @@
             this.width2.Name = "width2";
             this.width2.Size = new System.Drawing.Size(54, 20);
             this.width2.TabIndex = 1;
+            this.width2.Value = new decimal(new int[] {
+            650,
+            0,
+            0,
+            0});
             // 
             // label6
             // 
@@ -307,6 +330,11 @@
             this.height3.Name = "height3";
             this.height3.Size = new System.Drawing.Size(54, 20);
             this.height3.TabIndex = 2;
+            this.height3.Value = new decimal(new int[] {
+            364,
+            0,
+            0,
+            0});
             // 
             // label7
             // 
@@ -328,6 +356,11 @@
             this.width3.Name = "width3";
             this.width3.Size = new System.Drawing.Size(54, 20);
             this.width3.TabIndex = 1;
+            this.width3.Value = new decimal(new int[] {
+            260,
+            0,
+            0,
+            0});
             // 
             // label8
             // 
@@ -344,7 +377,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(447, 282);
             this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnStart);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -382,7 +415,7 @@
         private System.Windows.Forms.TextBox desDir;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.ProgressBar progressBar1;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnStart;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.GroupBox groupBox3;
         private System.Windows.Forms.Label label3;
--- ImageResizer/Form1.cs
+++ ImageResizer/Form1.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace ImageResizer
 {
     public partial class Form1 : Form
     {
+        private bool isCancelled = false;
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +23,128 @@ namespace ImageResizer
 
         private void button4_Click(object sender, EventArgs e)
         {
+            isCancelled = true;
             Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var folderDialog = new FolderBrowserDialog())
+            {
+                if (folderDialog.ShowDialog() == DialogResult.OK)
+                {
+                    sourceDir.Text = folderDialog.SelectedPath;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (var folderDialog = new FolderBrowserDialog())
+            {
+                if (folderDialog.ShowDialog() == DialogResult.OK)
+                {
+                    desDir.Text = folderDialog.SelectedPath;
+                }
+            }
+        }
+
+        private void ProcessImages(string sourceFolder, string destinationFolder, List<(int Width, int Height)> sizes)
+        {
+            Invoke((Action)(() => btnStart.Enabled = false));
+            var files = Directory.GetFiles(sourceFolder, "*.jpg");
+            int processedCount = 0;
+
+            foreach (var file in files)
+            {
+                if (isCancelled) break;
+                string productCode = Path.GetFileName(file).Split('_')[0];
+
+                foreach (var (width, height) in sizes)
+                {
+                    string resolutionFolder = Path.Combine(destinationFolder, productCode, $"{width}x{height}");
+                    Directory.CreateDirectory(resolutionFolder);
+
+                    string outputPath = Path.Combine(resolutionFolder, Path.GetFileName(file));
+
+                    // Check for existing file
+                    if (File.Exists(outputPath))
+                    {
+                        DialogResult result = MessageBox.Show(
+                            $"The file '{Path.GetFileName(file)}' already exists in the destination folder.\nDo you want to overwrite it?",
+                            "File Exists",
+                            MessageBoxButtons.YesNo,
+                            MessageBoxIcon.Warning
+                        );
+
+                        if (result == DialogResult.No)
+                        {
+                            continue; // Skip saving this file
+                        }
+                    }
+
+                    // Resize and save the image
+                    using (var originalImage = Image.FromFile(file))
+                    {
+                        var resizedImage = ResizeImage(originalImage, width, height);
+                        resizedImage.Save(outputPath, ImageFormat.Jpeg);
+                    }
+                }
+
+                processedCount++;
+                UpdateProgress(processedCount, files.Length);
+            }
+
+            Invoke((Action)(() =>
+            {
+                btnStart.Enabled = true;
+                MessageBox.Show($"Processed {processedCount} images.", "Completed", MessageBoxButtons.OK);
+            }));
+        }
+
+        private Image ResizeImage(Image original, int targetWidth, int targetHeight)
+        {
+            float ratio = Math.Min((float)targetWidth / original.Width, (float)targetHeight / original.Height);
+            int newWidth = (int)(original.Width * ratio);
+            int newHeight = (int)(original.Height * ratio);
+
+            var resized = new Bitmap(newWidth, newHeight);
+            using (var g = Graphics.FromImage(resized))
+            {
+                g.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+
+                g.DrawImage(original, 0, 0, newWidth, newHeight);
+            }
+
+            return resized;
+        }
+
+        private void UpdateProgress(int completed, int total)
+        {
+            Invoke((Action)(() =>
+            {
+                progressBar1.Maximum = total;
+                progressBar1.Value = completed;
+            }));
+        }
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(sourceDir.Text) || string.IsNullOrWhiteSpace(desDir.Text))
+            {
+                MessageBox.Show("Please select both source and destination folders.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            var sizes = new List<(int Width, int Height)>
+            {
+                ((int)width1.Value, (int)height1.Value),
+                ((int)width2.Value, (int)height2.Value),
+                ((int)width3.Value, (int)height3.Value)
+            };
+
+            Task.Run(() => ProcessImages(sourceDir.Text, desDir.Text, sizes));
         }
     }
 }
